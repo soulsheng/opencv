73ae6caa2aab1864d1a7aa799deebae29cec2540
soulsheng <soulsheng@qq.com>
2014-07-25 14:56:10 +0800

    time median filter/Blur, 1.8 ms for car.bmp (450,340,3)

--- samples/cpp/tutorial_code/ImgProc/Smoothing.cpp
+++ samples/cpp/tutorial_code/ImgProc/Smoothing.cpp
@@ -13,6 +13,9 @@
 using namespace std;
 using namespace cv;
 
+#include "timerC.h"
+#define		ENABLE_TIME_STEP	1 	// 测量某个范围的总时间
+
 /// Global Variables
 int DELAY_CAPTION = 1500;
 int DELAY_BLUR = 100;
@@ -41,7 +44,7 @@ int main( void )
   dst = src.clone();
   if( display_dst( DELAY_CAPTION ) != 0 ) { return 0; }
 
-
+#if 0
   /// Applying Homogeneous blur
   if( display_caption( "Homogeneous Blur" ) != 0 ) { return 0; }
 
@@ -56,22 +59,39 @@ int main( void )
   for ( int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2 )
       { GaussianBlur( src, dst, Size( i, i ), 0, 0 );
         if( display_dst( DELAY_BLUR ) != 0 ) { return 0; } }
+#endif
 
 
   /// Applying Median blur
   if( display_caption( "Median Blur" ) != 0 ) { return 0; }
 
   for ( int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2 )
-      { medianBlur ( src, dst, i );
-        if( display_dst( DELAY_BLUR ) != 0 ) { return 0; } }
+  { 
+
+		timerC  timeTest;
+#if ENABLE_TIME_STEP
+		timeTest.start();
+#endif
+		printf(" \n\nfilter window size = %d \n",3);
+		medianBlur ( src, dst, 3 );//	2.8 ms (450,340,3); 1.8 ms (520,540,3)
+
+#if ENABLE_TIME_STEP	// xx ms
+		timeTest.stop();
+		printf( "\n Time of medianBlur is : %.3f\n", timeTest.getTime() );
+#endif	
+
+        if( display_dst( DELAY_BLUR ) != 0 ) { return 0; } 
+  }
 
 
+#if 0
   /// Applying Bilateral Filter
   if( display_caption( "Bilateral Blur" ) != 0 ) { return 0; }
 
   for ( int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2 )
       { bilateralFilter ( src, dst, i, i*2, i/2 );
         if( display_dst( DELAY_BLUR ) != 0 ) { return 0; } }
+#endif
 
   /// Wait until user press a key
   display_caption( "End: Press a key!" );
---
+++ samples/cpp/tutorial_code/vc10/utility/timerC.h
@@ -0,0 +1,20 @@
+#ifndef TIMERCPP
+#define TIMERCPP
+
+#include <Windows.h>
+
+class timerC
+{
+public:
+	timerC();
+	void start();
+	void stop();
+	float getTime();
+
+private:
+	LARGE_INTEGER  freq;
+	LARGE_INTEGER  tStart, tStop;
+	float   differTime;
+};
+
+#endif
