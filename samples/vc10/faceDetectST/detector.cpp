ad9fb0e53dd25fd40227bdbabd631e55826ccb8b
s <s>
2015-01-05 15:45:38 +0800

    pack initialize detectST release

--- samples/vc10/faceDetectST/detector.cpp
+++ samples/vc10/faceDetectST/detector.cpp
@@ -16,14 +16,35 @@ using namespace sdktest;
 #define IMAGE_IN	"../data/bighero6.jpg"
 #define IMAGE_OUT	"../data/out.jpg"
 
-int main(int argc, char *argv[])
+ImageHelper *helper;
+mcv_handle_t hDetect;
+PMCV_FACERECT pface=NULL;
+unsigned int countFace=0;
+
+bool initialize()
 {
-	//assert(argc == 3);
-	ImageHelper *helper = new ImageHelperBackend();
+	helper = new ImageHelperBackend();
 
-	// init handle
-	mcv_handle_t hDetect = mcv_facesdk_create_multiview_detector_instance_from_resource(true, 2);
+	hDetect = mcv_facesdk_create_multiview_detector_instance_from_resource(true, 2);
 
+	return true;
+}
+
+bool release()
+{
+	delete helper;
+
+	// release the memory of result
+	mcv_facesdk_release_multiview_result(pface,countFace);
+
+	// destroy handle
+	mcv_facesdk_destroy_multiview_instance(hDetect);
+
+	return true;
+}
+
+bool detectST()
+{
 	Image *img=helper->LoadGrayImage(IMAGE_IN);
 	Image *img_color = helper->LoadRGBAImage(IMAGE_IN);
 	if(!img || !img_color){
@@ -32,38 +53,38 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
+	// detect
+	__TIC__();
+	mcv_facesdk_multiview_detector(hDetect,img->data,img->cols,img->rows,img->cols,&pface,&countFace);
+	__TOC__();
+
+	// draw result
+	for ( int i=0;i<countFace;i++){
+		helper->DrawRect(img_color,pface[i].Rect.top,pface[i].Rect.left,pface[i].Rect.right,pface[i].Rect.bottom, 3);
+		printf("face %d: %d %d %d %d\n", i, pface[i].Rect.top, pface[i].Rect.left, pface[i].Rect.right, pface[i].Rect.bottom);
+	}
+
+
+	helper->SaveImage(IMAGE_OUT,img_color);
+	helper->FreeImage(img);
+	helper->FreeImage(img_color);
+}
+
+int main(int argc, char *argv[])
+{
+	initialize();
 
 	cv::Mat in = cv::imread( IMAGE_IN );
 	cv::imshow( "in", in );
 	cv::waitKey( );
 
-	// detect
-	PMCV_FACERECT pface=NULL;
-	unsigned int count;
-	__TIC__();
-	mcv_facesdk_multiview_detector(hDetect,img->data,img->cols,img->rows,img->cols,&pface,&count);
-	__TOC__();
-
-	// draw result
-	for ( int i=0;i<count;i++){
-		helper->DrawRect(img_color,pface[i].Rect.top,pface[i].Rect.left,pface[i].Rect.right,pface[i].Rect.bottom, 3);
-		printf("face %d: %d %d %d %d\n", i, pface[i].Rect.top, pface[i].Rect.left, pface[i].Rect.right, pface[i].Rect.bottom);
-	}
-
-	// release the memory of result
-	mcv_facesdk_release_multiview_result(pface,count);
-
-	// destroy handle
-    mcv_facesdk_destroy_multiview_instance(hDetect);
-        
-	helper->SaveImage(IMAGE_OUT,img_color);
-	helper->FreeImage(img);
-	helper->FreeImage(img_color);
-	delete helper;
+	detectST();
 
 	cv::Mat out = cv::imread( IMAGE_OUT );
 	cv::imshow( "out", out );
 	cv::waitKey( );
+
+	release();
 
 	return 0;
 }
